7281458e399bf91ed4f65db8c920dd562c611ead
Homura <egzozu.be.bas@gmail.com>
2026-07-03 14:06:35 +0300

    p

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -375,10 +375,31 @@ class AndroidPlatform extends PlatformTarget
 		var x86 = (command == "rebuild" || ArrayTools.containsValue(project.architectures, Architecture.X86));
 		var x64 = (/*command == "rebuild" ||*/ ArrayTools.containsValue(project.architectures, Architecture.X64));
 
+		if (project.targetFlags.exists("ONLY_ARM64"))
+		{
+			arm64 = true;
+			armv7 = x86 = x64 = false;
+		}
+		else if (project.targetFlags.exists("ONLY_ARMV7"))
+		{
+			armv7 = true;
+			arm64 = x86 = x64 = false;
+		}
+		else if (project.targetFlags.exists("ONLY_X86_64"))
+		{
+			x64 = true;
+			arm64 = armv7 = x86 = false;
+		}
+		else if (project.targetFlags.exists("ONLY_X86"))
+		{
+			x86 = true;
+			arm64 = armv7 = x64 = false;
+		}
+
 		var commands = [];
 
 		if (armv5) commands.push(["-Dandroid", "-DPLATFORM=android-21"]);
-		if (armv7) commands.push(["-Dandroid", "-DHXCPP_ARMV7", "-DHXCPP_ARM7", "-DPLATFORM=android-21"]);
+		if (armv7) commands.push(["-Dandroid", "-DHXCPP_ARMV7", "-DHXCPP_ARMV7", "-DPLATFORM=android-21"]);
 		if (arm64) commands.push(["-Dandroid", "-DHXCPP_ARM64", "-DPLATFORM=android-21"]);
 		if (x86) commands.push(["-Dandroid", "-DHXCPP_X86", "-DPLATFORM=android-21"]);
 		if (x64) commands.push(["-Dandroid", "-DHXCPP_X86_64", "-DPLATFORM=android-21"]);
